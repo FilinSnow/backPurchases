253a1090c75d4d22f45f7a00fc547fd4b0431754
Vlad <vproskurin.exceedteam@gmail.com>
2021-09-07 15:47:31 +0300

    Add files backend

---
+++ src/modules/controllers/purchases.controller.js
@@ -0,0 +1,47 @@
+const Purchases = require('../../db/models/purchases/index');
+const _ = require('underscore');
+
+module.exports.getAllPurchases = (req, res) => {
+  Purchases.find().then(result => {
+    res.send({ data: result });
+  }).catch(err => new Error(err));
+}
+
+module.exports.createNewPurchases = (req, res) => {
+  console.log('req.body', req.body)
+  if (Object.keys(req.body).length == 0) {
+    return res.send('Not send data');
+  }
+  _.mapObject(req.body, (val, key) => {
+    if (val == '') {
+      return res.send('You have not entered data');
+    }
+  })
+  const purchase = new Purchases(req.body);
+  purchase.save().then(r => {
+    res.send(r);
+  }).catch(err => new Error(err));
+}
+
+module.exports.updatePurchasesInfo = (req, res) => {
+  if (Object.keys(req.body).length == 0) {
+    return res.send('Not send data');
+  }
+  const { _id, text, date, price } = req.body;
+  Purchases.updateOne(
+    { _id },
+    { text, date, price },
+  ).then(r => {
+    res.send(r);
+  }).catch(err => new Error(err));
+}
+
+module.exports.deletePurchase = (req, res) => {
+  console.log(req.params);
+  Purchases.deleteOne({
+    _id: req.params.id
+  }
+  ).then(r => {
+    res.send(r);
+  }).catch(err => new Error(err));
+}
