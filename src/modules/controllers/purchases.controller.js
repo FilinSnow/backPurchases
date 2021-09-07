135f7849fbe31cf726f8cf7600884980b1dd93e1
Vlad <vproskurin.exceedteam@gmail.com>
2021-09-07 17:12:29 +0300

    Fix

--- src/modules/controllers/purchases.controller.js
+++ src/modules/controllers/purchases.controller.js
@@ -8,23 +8,22 @@ module.exports.getAllPurchases = (req, res) => {
 }
 
 module.exports.createNewPurchases = (req, res) => {
-  console.log('req.body', req.body)
-  if (Object.keys(req.body).length == 0) {
+  if (Object.keys(req.body).length === 0) {
     return res.send('Not send data');
   }
   _.mapObject(req.body, (val, key) => {
-    if (val == '') {
+    if (!val) {
       return res.send('You have not entered data');
     }
   })
   const purchase = new Purchases(req.body);
   purchase.save().then(r => {
     res.send(r);
-  }).catch(err => new Error(err));
+  }).catch(err => res.send(new Error(err)));
 }
 
 module.exports.updatePurchasesInfo = (req, res) => {
-  if (Object.keys(req.body).length == 0) {
+  if (Object.keys(req.body).length === 0) {
     return res.send('Not send data');
   }
   const { _id, text, date, price } = req.body;
@@ -33,15 +32,18 @@ module.exports.updatePurchasesInfo = (req, res) => {
     { text, date, price },
   ).then(r => {
     res.send(r);
-  }).catch(err => new Error(err));
+  }).catch(err => res.send(new Error(err)));
 }
 
 module.exports.deletePurchase = (req, res) => {
-  console.log(req.params);
-  Purchases.deleteOne({
-    _id: req.params.id
+  if (req.params.id) {
+    Purchases.deleteOne({
+      _id: req.params.id
+    }
+    ).then(r => {
+      res.send(r);
+    }).catch(err => res.send(new Error(err)));
+  } else {
+    res.send('Not id')
   }
-  ).then(r => {
-    res.send(r);
-  }).catch(err => new Error(err));
 }
